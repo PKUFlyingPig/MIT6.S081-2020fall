ef8d47a751213be422d15c7bfee47aca4926d623
Frans Kaashoek <kaashoek@mit.edu>
2020-10-23 08:35:36 -0400

    fs lab

--- kernel/param.h
+++ kernel/param.h
@@ -1,4 +1,8 @@
-#define NPROC        64  // maximum number of processes
+#ifdef LAB_FS
+#define NPROC        10  // maximum number of processes
+#else
+#define NPROC        64  // maximum number of processes (speedsup bigfile)
+#endif
 #define NCPU          8  // maximum number of CPUs
 #define NOFILE       16  // open files per process
 #define NFILE       100  // open files per system
@@ -9,5 +13,7 @@
 #define MAXOPBLOCKS  10  // max # of blocks any FS op writes
 #define LOGSIZE      (MAXOPBLOCKS*3)  // max data blocks in on-disk log
 #define NBUF         (MAXOPBLOCKS*3)  // size of disk block cache
-#define FSSIZE       1000  // size of file system in blocks
+#define FSSIZE       200000  // size of file system in blocks
 #define MAXPATH      128   // maximum file path name
+
+
