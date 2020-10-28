82bc3d5ee6564de79889bdd146c580399bdf04e6
Frans Kaashoek <kaashoek@mit.edu>
2020-10-28 19:44:37 -0400

    mmap lab

--- kernel/fcntl.h
+++ kernel/fcntl.h
@@ -3,3 +3,13 @@
 #define O_RDWR    0x002
 #define O_CREATE  0x200
 #define O_TRUNC   0x400
+
+#ifdef LAB_MMAP
+#define PROT_NONE       0x0
+#define PROT_READ       0x1
+#define PROT_WRITE      0x2
+#define PROT_EXEC       0x4
+
+#define MAP_SHARED      0x01
+#define MAP_PRIVATE     0x02
+#endif
