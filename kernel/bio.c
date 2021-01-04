d660b5dda1c3a3c3c73fb9b9bdba2e9716d43ff4
FlyingPig <1800012735@pku.edu.cn>
2021-01-04 17:51:54 +0800

    lock lab finished

--- kernel/bio.c
+++ kernel/bio.c
@@ -25,12 +25,14 @@
 
 struct {
   struct spinlock lock;
-  struct buf buf[NBUF];
+  struct buf head[NBUCKET];
+  struct buf hash[NBUCKET][NBUF];
+  struct spinlock hashlock[NBUCKET]; // lock per bucket
 
   // Linked list of all buffers, through prev/next.
   // Sorted by how recently the buffer was used.
   // head.next is most recent, head.prev is least.
-  struct buf head;
+  // struct buf head;
 } bcache;
 
 void
@@ -39,16 +41,19 @@ binit(void)
   struct buf *b;
 
   initlock(&bcache.lock, "bcache");
+  for (int i = 0; i < NBUCKET; i++) {
+    initlock(&bcache.hashlock[i], "bcache");
 
-  // Create linked list of buffers
-  bcache.head.prev = &bcache.head;
-  bcache.head.next = &bcache.head;
-  for(b = bcache.buf; b < bcache.buf+NBUF; b++){
-    b->next = bcache.head.next;
-    b->prev = &bcache.head;
-    initsleeplock(&b->lock, "buffer");
-    bcache.head.next->prev = b;
-    bcache.head.next = b;
+    // Create linked list of buffers
+    bcache.head[i].prev = &bcache.head[i];
+    bcache.head[i].next = &bcache.head[i];
+    for(b = bcache.hash[i]; b < bcache.hash[i]+NBUF; b++){
+      b->next = bcache.head[i].next;
+      b->prev = &bcache.head[i];
+      initsleeplock(&b->lock, "buffer");
+      bcache.head[i].next->prev = b;
+      bcache.head[i].next = b;
+    }
   }
 }
 
@@ -60,13 +65,14 @@ bget(uint dev, uint blockno)
 {
   struct buf *b;
 
-  acquire(&bcache.lock);
+  uint hashcode = blockno % NBUCKET;
+  acquire(&bcache.hashlock[hashcode]);
 
   // Is the block already cached?
-  for(b = bcache.head.next; b != &bcache.head; b = b->next){
+  for(b = bcache.head[hashcode].next; b != &bcache.head[hashcode]; b = b->next){
     if(b->dev == dev && b->blockno == blockno){
       b->refcnt++;
-      release(&bcache.lock);
+      release(&bcache.hashlock[hashcode]);
       acquiresleep(&b->lock);
       return b;
     }
@@ -74,13 +80,13 @@ bget(uint dev, uint blockno)
 
   // Not cached.
   // Recycle the least recently used (LRU) unused buffer.
-  for(b = bcache.head.prev; b != &bcache.head; b = b->prev){
+  for(b = bcache.head[hashcode].prev; b != &bcache.head[hashcode]; b = b->prev){
     if(b->refcnt == 0) {
       b->dev = dev;
       b->blockno = blockno;
       b->valid = 0;
       b->refcnt = 1;
-      release(&bcache.lock);
+      release(&bcache.hashlock[hashcode]);
       acquiresleep(&b->lock);
       return b;
     }
@@ -121,19 +127,20 @@ brelse(struct buf *b)
 
   releasesleep(&b->lock);
 
-  acquire(&bcache.lock);
+  uint hashcode = b->blockno % NBUCKET;
+  acquire(&bcache.hashlock[hashcode]);
   b->refcnt--;
   if (b->refcnt == 0) {
     // no one is waiting for it.
     b->next->prev = b->prev;
     b->prev->next = b->next;
-    b->next = bcache.head.next;
-    b->prev = &bcache.head;
-    bcache.head.next->prev = b;
-    bcache.head.next = b;
+    b->next = bcache.head[hashcode].next;
+    b->prev = &bcache.head[hashcode];
+    bcache.head[hashcode].next->prev = b;
+    bcache.head[hashcode].next = b;
   }
   
-  release(&bcache.lock);
+  release(&bcache.hashlock[hashcode]);
 }
 
 void
--- kernel/spinlock.c
+++ kernel/spinlock.c
@@ -9,7 +9,7 @@
 #include "defs.h"
 
 #ifdef LAB_LOCK
-#define NLOCK 500
+#define NLOCK 600
 
 static struct spinlock *locks[NLOCK];
 struct spinlock lock_locks;
