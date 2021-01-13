fb40a222d0edbcfc23f20b8ce13f301c9e3f6100
FlyingPig <1800012735@pku.edu.cn>
2021-01-13 10:59:03 +0800

    add syscall

--- user/user.h
+++ user/user.h
@@ -23,6 +23,9 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+void *mmap(void *addr, uint length, int prot, int flags,
+           int fd, uint offset);
+int munmap(void *addr, uint len);
 
 // ulib.c
 int stat(const char*, struct stat*);
--- user/usys.pl
+++ user/usys.pl
@@ -36,3 +36,6 @@ entry("getpid");
 entry("sbrk");
 entry("sleep");
 entry("uptime");
+entry("mmap");
+entry("munmap");
+
